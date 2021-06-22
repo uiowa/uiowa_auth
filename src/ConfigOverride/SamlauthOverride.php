11d6f99bdb1785f9d7db760d49a864dcf7068f53
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-06-22 15:56:32 -0500

    Override samlauth config to make all roles linkable

---
+++ src/ConfigOverride/SamlauthOverride.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\uiowa_auth\ConfigOverride;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+use Drupal\user\Entity\Role;
+
+/**
+ * Samlauth configuration overrides.
+ */
+class SamlauthOverride implements ConfigFactoryOverrideInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    $overrides = [];
+
+    // Allow all roles to be linkable.
+    if (in_array('samlauth.authentication', $names)) {
+      $roles = user_roles(TRUE);
+      $allowed = [];
+
+      /** @var Role $role */
+      foreach ($roles as $role) {
+          $allowed[$role->id()] = $role->id();
+      }
+
+      $overrides['samlauth.authentication']['map_user_roles'] = $allowed;
+    }
+
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'SamlauthOverride';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
+}
